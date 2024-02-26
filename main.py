6eb7d6255e2c9a55942302ea18d7437045e47ea9
Abhishek Roy <abhi.roy15@gmail.com>
2024-02-26 23:24:32 +0000

    fix: modified few print statements

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import StandardScaler
 
 def remove_outliers(df):
     numerical_cols = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
-    print("Numerical columns:", numerical_cols)
     for col in numerical_cols:
         Q1 = df[col].quantile(0.25)
         Q3 = df[col].quantile(0.75)
@@ -24,24 +23,23 @@ def feature_normalization(df):
 def main():
     data = pd.read_csv('diabetic_data.csv')
 
-    print("Initial shape of the data:", data.shape)
+    print("Shape of the data:\n", data.shape)
     
     data.drop('encounter_id', axis=1, inplace=True)
     
     missing_values_before = data.isnull().sum()
-    print("Summary of missing values before replace:\n", missing_values_before)
+    print("\nSummary of missing values before replace:\n", missing_values_before)
     
     data.replace('?', np.nan, inplace=True)
 
     missing_values_after = data.isnull().sum()
-    print("Summary of missing values after replace:\n", missing_values_after)
+    print("\nSummary of missing values after replace:\n", missing_values_after)
 
     data['readmitted'] = data['readmitted'].map({'<30': 1, '>30': 0, 'NO': 0})
     
-    print("Data types of each column:\n", data.dtypes)
+    print("\nData types of each column:\n", data.dtypes)
 
     missing_percent = data.isnull().mean() * 100
-    print("Percentage of missing values:\n", missing_percent)
     
     columns_to_drop = missing_percent[missing_percent > 90].index
     data.drop(columns=columns_to_drop, inplace=True)
@@ -56,7 +54,7 @@ def main():
     
     data.dropna(inplace=True)
     
-    print("Summary statistics of numerical columns:\n", data.describe())
+    print("\nSummary statistics of numerical columns:\n", data.describe())
     
     # Removing outliers
     data = remove_outliers(data)
@@ -64,7 +62,7 @@ def main():
     # Feature normalization
     data = feature_normalization(data)
     
-    print("Final shape of the data:", data.shape)
+    print("\nFinal shape of the data:\n", data.shape)
 
 
 if __name__ == '__main__':
