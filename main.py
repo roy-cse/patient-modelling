d8a56b9f7043d220e140447e31e1b8b47d6fec71
Soumik Datta <soumikdr@gmail.com>
2024-03-26 21:20:26 +0000

    refactor: added stratify to train_test_split

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def evaluate_model_performance(data):
     y = data['readmitted']
 
     # Splitting the dataset into training and testing set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
     # Feature selection with RFE
     model = LogisticRegression(max_iter=1000, solver='liblinear')
